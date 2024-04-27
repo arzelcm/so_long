8a63eb2d40eb0464bd3803beb478af488ba90547
arzelcm <arzelcanavate@gmail.com>
2024-04-27 22:30:14 +0200

    Update loader style

--- src/loader.c
+++ src/loader.c
@@ -6,7 +6,7 @@
 /*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:06:36 by arcanava          #+#    #+#             */
-/*   Updated: 2024/04/27 17:34:01 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/04/27 19:51:50 by arcanava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	update_loading(char *message, int value)
 {
-	ft_printf("\033[1A\033[2K%s --> [%i%%]\n", message, value);
+	ft_printf("\033[1A\033[2K%s --> [%i%%]\n", message, ft_normalize(value, 0, 100));
 }
